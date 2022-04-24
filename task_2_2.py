da2d119bd16833890ed06a3d1e72fb8d54bfd7ab
Solmyrr <98647378+Solmyrr@users.noreply.github.com>
2022-04-24 20:19:37 +0300

    Add files via upload
    New files

--- task_2_2.py
+++ task_2_2.py
@@ -3,26 +3,18 @@ print(phraze)
 numbers = []
 for word in phraze:
     if word.isdigit():
-        numbers.extend(f'"{int(word):02}"')
-    elif word.startswith('+') or word.startswith('+') and word[1].isdigit():
-        numbers.extend(f'"{word[0]}{int(word):02}"')
+        numbers.extend(['"', f'{int(word):02}', '"'])
+    elif (word.startswith('+') or (word.startswith('-')) and word[1:].isdigit()):
+        numbers.extend(['"', f'{word[0]}{int(word[1:]):02}', '"'])
     else:
          numbers.append(word)
-edit_phraze = " ".join(numbers)
+edit_phraze = ' '.join(numbers)
 print(edit_phraze)
-for
+
+# Разобралась с расчленением цифр, но удалить пробелы так и не смогла...
 
 
 
 
 
-# hour = 5
-# min = 17
-# temp = 5
-# fraze = f'в "{hour:02d}" часов "{min}" минут температура воздуха была "+{temp:02d}" градусов'
-
-
-# пока не совсем понимаю, какими методами можно решать дз. Сделала в лоб. Думаю, что неправильно.
-# хотелось бы разбирать на уроке задачи хотя бы отдаленно напоминающие ДЗ по уровню сложности,
-# чтобы выработать какой-то алгоритм действий
-# Ощущаю растерянность перед выполнением заданий. Как буд-то патроны дали, а ружье забыли :)
+# https: // github.com / Solmyrr / Voronova_Darya_Python / pull / 1
